4064a3983c3a89867798f566d1443dc66928d761
wlchrist <wchristian2000@gmail.com>
2023-04-18 14:25:44 -0700

    1 binaries uploaded

---
+++ src/Navbar.jsx
@@ -0,0 +1,36 @@
+// src/Navbar.jsx
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand"></div>
+      <div className="navbar-menu-group">
+        <ul className="navbar-menu">
+          <li className="navbar-menu-item">
+            <a href="/">Stats</a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/about">Leaderboard</a>
+          </li>
+          <li className="navbar-menu-item">
+            <a href="/contact">FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-account">
+        <ul>
+          <li className="navbar-menu-item-account">
+            <a href="">Register</a>
+          </li>
+          <li className="navbar-menu-item-account">
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
